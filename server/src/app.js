ba96c9c58e7ca236cd087abe9299f784e9557355
Fayiz <tvfayiztv@gmail.com>
2026-05-22 15:55:09 +0530

    Test router completed

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import testRoutes from "./routes/testRoutes.js";
 
 const app = express();
 app.use(express.json());
+
+app.use("/api/test", testRoutes);
 
 export default app;
