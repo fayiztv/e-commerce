7924cdea06ee9b63923400e8f6357a1498f093d3
Fayiz <tvfayiztv@gmail.com>
2026-05-22 15:59:57 +0530

    User register API completed

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,21 +1,15 @@
 import express from "express";
 import {
-  getProfile,
-  userLogin,
   userRegister,
 } from "../controllers/userControllers.js";
-import { protect } from "../middlewares/authMiddlewares.js";
 import { formValidator } from "../validations/authValidations.js";
 
 const router = express.Router();
 
-router.post("/login", formValidator(["email", "password"]), userLogin);
 router.post(
   "/register",
   formValidator(["name", "email", "password"]),
   userRegister,
 );
-
-router.get("/profile", protect, getProfile);
 
 export default router;
