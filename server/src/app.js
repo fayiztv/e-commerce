750612ed70542d77f32df1087833494ecccf9c47
Fayiz <tvfayiztv@gmail.com>
2026-05-25 16:23:31 +0530

    Product api createdin App.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import testRoutes from "./routes/testRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import adminRoutes from "./routes/adminRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import { errorHandler } from "./middlewares/errorMiddleware.js";
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use("/api/test", testRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 
 app.use(errorHandler);
