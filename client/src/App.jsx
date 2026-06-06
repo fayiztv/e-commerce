5fbf096389aab6614b3edc587f7df3d9ed855901
Fayiz <tvfayiztv@gmail.com>
2026-06-06 14:13:17 +0530

    Frontend created

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h1>Hello</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
