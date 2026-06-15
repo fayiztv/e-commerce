a89833774c3efe7a62ce7df4b086e9fa9853b67b
Fayiz <tvfayiztv@gmail.com>
2026-06-15 17:34:29 +0530

    admin product card updated

--- client/src/components/admin/AdminProductCard.jsx
+++ client/src/components/admin/AdminProductCard.jsx
@@ -1,35 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
 export default function AdminProductCard() {
+  const navigate = useNavigate();
+  function handleEditClick() {
+    navigate(`:id`);
+  }
   return (
-    <div className="flex justify-around">
+    <div onClick={handleEditClick}>
       {/* Image */}
-      <div>
-        <div className="h-64 bg-gray-200"></div>
-      </div>
+      <div className="h-64 bg-gray-200"></div>
 
       {/* Content */}
-      <div className="flex justify-around">
-        <div className="space-y-3 p-5">
+      <div className="space-y-4 p-5">
+        <div>
           <h2 className="text-xl font-bold">iPhone 16 Pro Max</h2>
 
           <p className="text-gray-500">Electronics</p>
+        </div>
 
-          <p className="font-semibold">₹149999</p>
+        <div className="flex items-center justify-between">
+          <span className="text-xl font-bold">₹1,49,999</span>
 
-          <p className="text-sm text-gray-500">Stock: 15</p>
-
-          <span className="rounded-full bg-green-100 px-3 py-1 text-sm text-green-700">
+          <span className="rounded-full bg-green-100 px-3 py-1 text-sm text-green-600">
             Active
           </span>
         </div>
 
-        <div className="mt-5 flex flex-col gap-3">
-          <button className="flex-1 rounded-lg border py-3">View</button>
+        <div className="text-gray-500">Stock: 12</div>
 
-          <button className="flex-1 rounded-lg bg-blue-500 py-3 text-white">
+        <div className="flex gap-3">
+          <button
+            onClick={handleEditClick}
+            className="
+                    flex-1
+                    rounded-lg
+                    bg-blue-500
+                    py-3
+                    text-white
+                    transition
+                    hover:bg-blue-600
+                  "
+          >
             Edit
           </button>
 
-          <button className="flex-1 rounded-lg bg-red-500 py-3 text-white">
+          <button
+            className="
+                    flex-1
+                    rounded-lg
+                    bg-red-500
+                    py-3
+                    text-white
+                    transition
+                    hover:bg-red-600
+                  "
+          >
             Delete
           </button>
         </div>
--- client/src/pages/admin/AdminProducts.jsx
+++ client/src/pages/admin/AdminProducts.jsx
@@ -1,14 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import AdminProductCard from "../../components/admin/AdminProductCard";
 
 export default function AdminProducts() {
   const navigate = useNavigate();
 
   function handleAddClick() {
     navigate(`new`);
-  }
-
-  function handleEditClick() {
-    navigate(`:id`);
   }
   return (
     <div className="space-y-8">
@@ -44,58 +41,7 @@ export default function AdminProducts() {
               hover:shadow-lg
             "
           >
-            {/* Image */}
-            <div className="h-64 bg-gray-200"></div>
-
-            {/* Content */}
-            <div className="space-y-4 p-5">
-              <div>
-                <h2 className="text-xl font-bold">iPhone 16 Pro Max</h2>
-
-                <p className="text-gray-500">Electronics</p>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <span className="text-xl font-bold">₹1,49,999</span>
-
-                <span className="rounded-full bg-green-100 px-3 py-1 text-sm text-green-600">
-                  Active
-                </span>
-              </div>
-
-              <div className="text-gray-500">Stock: 12</div>
-
-              <div className="flex gap-3">
-                <button
-                  onClick={handleEditClick}
-                  className="
-                    flex-1
-                    rounded-lg
-                    bg-blue-500
-                    py-3
-                    text-white
-                    transition
-                    hover:bg-blue-600
-                  "
-                >
-                  Edit
-                </button>
-
-                <button
-                  className="
-                    flex-1
-                    rounded-lg
-                    bg-red-500
-                    py-3
-                    text-white
-                    transition
-                    hover:bg-red-600
-                  "
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
+            <AdminProductCard/>
           </div>
         ))}
       </div>
