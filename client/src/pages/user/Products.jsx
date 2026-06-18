b7346ecdda0f30bc49b9fb2853e122a47828f031
Fayiz <tvfayiztv@gmail.com>
2026-06-19 02:42:54 +0530

    Products listed in the products page

--- client/src/pages/user/Products.jsx
+++ client/src/pages/user/Products.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
 import ProductCard from "../../components/user/ProductCard";
+import { getProductsApi } from "../../services/productService";
+import { getCategoriesApi } from "../../services/categoryServices";
 
 export default function ProductsPage() {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await getProductsApi();
+      setProducts(res.data);
+    };
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      const res = await getCategoriesApi();
+      setCategories(res.data);
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <div className="mx-auto max-w-7xl px-4 py-10">
-
       {/* Page Title */}
       <div className="mb-10 flex items-center justify-between">
-        <h1 className="text-3xl font-bold">
-          Products
-        </h1>
+        <h1 className="text-3xl font-bold">Products</h1>
 
         <select className="rounded-lg border px-4 py-2 outline-none">
           <option>Latest</option>
@@ -18,57 +37,34 @@ export default function ProductsPage() {
       </div>
 
       <div className="flex flex-col gap-8 lg:flex-row">
-
         {/* Sidebar */}
         <div className="w-full rounded-2xl border p-6 lg:w-64">
-
-          <h2 className="mb-5 text-xl font-semibold">
-            Filters
-          </h2>
+          <h2 className="mb-5 text-xl font-semibold">Filters</h2>
 
           {/* Categories */}
           <div className="mb-8">
-            <h3 className="mb-3 font-medium">
-              Categories
-            </h3>
+            <h3 className="mb-3 font-medium">Categories</h3>
 
             <div className="space-y-3">
-
-              <label className="flex gap-2">
-                <input type="checkbox" />
-                Clothing
-              </label>
-
-              <label className="flex gap-2">
-                <input type="checkbox" />
-                Electronics
-              </label>
-
-              <label className="flex gap-2">
-                <input type="checkbox" />
-                Accessories
-              </label>
-
+              {categories?.map((item) => (
+                <label key={item._id} className="flex gap-2">
+                  <input type="checkbox" />
+                  {item.name}
+                </label>
+              ))}
             </div>
           </div>
-
         </div>
 
         {/* Product Grid */}
         <div className="flex-1">
-
           <div className="grid gap-8 sm:grid-cols-2 xl:grid-cols-3">
-
-            {[1, 2, 3, 4, 5, 6].map((item) => (
-              <ProductCard item={item}/>
+            {products?.map((item) => (
+              <ProductCard key={item._id} item={item} />
             ))}
-
           </div>
-
         </div>
-
       </div>
-
     </div>
   );
 }
