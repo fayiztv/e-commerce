0a9f50e3886ec59ade21130bcd5644be0577d673
Fayiz <tvfayiztv@gmail.com>
2026-06-19 02:43:27 +0530

    Product card updated

--- client/src/components/user/ProductCard.jsx
+++ client/src/components/user/ProductCard.jsx
@@ -1,7 +1,11 @@
-export default function ProductCard({item}) {
+import { useNavigate } from "react-router-dom";
+
+export default function ProductCard({ item }) {
+  const inStock = item.stock > 0;
+  const navigate = useNavigate();
   return (
     <div
-      key={item}
+      onClick={() => navigate(`/product/${item._id}`)}
       className="overflow-hidden rounded-2xl border bg-white shadow-sm transition hover:-translate-y-1 hover:shadow-lg"
     >
       {/* Image */}
@@ -9,18 +13,24 @@ export default function ProductCard({item}) {
 
       {/* Content */}
       <div className="p-5">
-        <h2 className="mb-2 text-lg font-semibold">Product Name</h2>
+        <h2 className="mb-2 text-lg font-semibold">{item?.name}</h2>
 
-        <p className="mb-1 text-gray-500">Electronics</p>
+        <p className="mb-1 text-gray-500">{item?.category?.name}</p>
 
         <div className="mb-5 flex items-center justify-between">
-          <span className="text-xl font-bold">₹999</span>
-
-          <span className="text-sm text-green-600">In Stock</span>
+          <span className="text-xl font-bold">₹{item.price}</span>
+          {inStock ? (
+            <span className="text-sm text-green-600">In Stock</span>
+          ) : (
+            <span className="text-sm text-red-600">Out of Stock</span>
+          )}
         </div>
 
         <div className="flex gap-3">
-          <button className="flex-1 rounded-lg bg-black py-3 text-white transition hover:bg-gray-800">
+          <button
+            disabled={!inStock}
+            className="flex-1 rounded-lg bg-black py-3 text-white transition hover:bg-gray-800 disabled:bg-gray-400"
+          >
             Add To Cart
           </button>
 
